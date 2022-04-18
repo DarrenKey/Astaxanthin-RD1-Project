d769c7c198b996bee8add0c57c15b540193e3da3
Darren Key <dyk34@cornell.edu>
2022-04-18 15:05:35 -0400

    Moved files around

--- finding_coverage/entire_coverage.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplot as plt
-
-plt.close("all")
-
-df = pd.read_csv("rd1_genome.coverage", sep="\t")
-
-print(f"Summary: { df['Coverage'].describe() }")
